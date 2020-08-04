edd13d0bbc534e311adcb82a0ce4aad19bcf5fc9
purnareddys <33196988+purnareddys@users.noreply.github.com>
2020-08-04 16:29:28 +0530

    add basic routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import Cart from "./pages/Cart";
 import Photos from "./pages/Photos";
-
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Home Page</h1>
+      <Switch>
+        <Route exact path="/">
+          <Photos />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
     </div>
   );
 }
